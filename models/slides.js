8c3033b0e215ffb367535d3d11001b09156f3009
Natalija-Virt <virtnatalie@gmail.com>
2017-12-06 11:02:22 +0200

    15

--- models/slides.js
+++ models/slides.js
@@ -4,16 +4,16 @@ var ImageSchema = mongoose.Schema({
     image: String
 });
 
-var Image = mongoose.model('Image', ImageSchema);
+var Images = mongoose.model('Image', ImageSchema);
 
-Image.getImage = (cb) => {
-    console.log('module.export c');
+Images.getImage = (cb) => {
+    //console.log('module.export c');
     res.send('module.export s');
-    Image.find(cb);
+    //Image.find(cb);
 };
 
-Image.addImage = (row, cb) => {
-    Image.create(row, cb);
+Images.addImage = (row, cb) => {
+    Images.create(row, cb);
 };
 
-module.exports = Image;
+module.exports = Images;
